d32dab061a2305b9b57a984ccd8d9bf39167be95
Asa Sprow <66881186+TheSecondReal0@users.noreply.github.com>
2021-01-25 19:18:23 -0500

    More tile work

--- src/games/pawn_game/resources/tiletype/tiletype.gd
+++ src/games/pawn_game/resources/tiletype/tiletype.gd
@@ -3,4 +3,7 @@ extends Resource
 class_name TileType
 
 export (String) var name
-export (bool) var passable
+export (bool) var walkable
+export (Texture) var texture
+export (Color) var modulate = Color(1,1,1,1)
+export (Vector2) var texture_scale = Vector2(1,1)
