4ef7d7c09f6dc19b81fc04fd4334758bdb2594a0
TheSecondReal0 <66881186+TheSecondReal0@users.noreply.github.com>
2021-01-29 17:22:00 -0500

    Update map.gd

--- src/games/pawn_game/map_editor/map.gd
+++ src/games/pawn_game/map_editor/map.gd
@@ -1,2 +1,21 @@
 extends Node2D
 
+var tile_resources: Dictionary
+# {Vector2: tile type name}
+var map_tiles: Dictionary = {}
+
+signal tile_changed(pos, old_type, new_type)
+
+func place_tiles(start_pos: Vector2, end_pos: Vector2, type: String):
+	pass
+
+func place_tile(pos: Vector2, type: String):
+	var old_type
+	if map_tiles.has(pos):
+		old_type = map_tiles[pos]
+	else:
+		old_type = null
+	emit_signal("tile_changed", pos, old_type, type)
+
+func round_pos(pos: Vector2, step: int = 20) -> Vector2:
+	return Vector2(stepify(pos.x, step), stepify(pos.y, step))
