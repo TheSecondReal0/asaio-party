cc367a3e439bd485b8ad952a20a91773005698e3
Asa Sprow <66881186+TheSecondReal0@users.noreply.github.com>
2021-01-29 20:04:57 -0500

    saving maps to json groundwork

--- src/games/pawn_game/map_editor/map.gd
+++ src/games/pawn_game/map_editor/map.gd
@@ -9,10 +9,16 @@ var map_tile_nodes: Dictionary = {}
 signal tile_changed(pos, old_type, new_type)
 
 func _ready():
+# warning-ignore:return_value_discarded
 	get_parent().connect("tile_placed", self, "place_tile")
 
-func place_tiles(start_pos: Vector2, end_pos: Vector2, type: String):
-	pass
+func _process(delta):
+	if Input.is_action_just_pressed("r"):
+		print(map_tiles)
+		var json = tiles_to_json(map_tiles)
+		print(json)
+		var tiles = json_to_tiles(json)
+		print(tiles)
 
 func place_tile(pos: Vector2, type: String):
 	if not tile_resources.has(type):
@@ -35,6 +41,28 @@ func create_tile(pos: Vector2, type: String) -> Node:
 	add_child(new_tile)
 	new_tile.global_position = pos
 	return new_tile
+
+func tiles_to_json(tiles: Dictionary) -> String:
+	var json_tiles: Dictionary = {}
+	for coord in map_tiles.keys():
+		json_tiles[coord_to_str(coord)] = map_tiles[coord]
+	var json_str: String = JSON.print(json_tiles)
+	return json_str
+
+func json_to_tiles(json: String) -> Dictionary:
+	var json_tiles: Dictionary = JSON.parse(json).result
+	var tile_dict: Dictionary = {}
+	for str_coord in json_tiles:
+		tile_dict[str_to_coord(str_coord)] = json_tiles[str_coord]
+	return tile_dict
+
+func coord_to_str(coord: Vector2) -> String:
+	return str(coord.x) + "," + str(coord.y)
+
+func str_to_coord(string: String) -> Vector2:
+	var split = string.split(",")
+	return Vector2(int(split[0]), int(split[1]))
+	return Vector2()
 
 func round_pos(pos: Vector2, step: int = 20) -> Vector2:
 	return Vector2(stepify(pos.x, step), stepify(pos.y, step))
