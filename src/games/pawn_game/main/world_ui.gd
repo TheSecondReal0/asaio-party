6ff796c0c1126b22757916c0e9fe0f50bb93d862
Asa Sprow <66881186+TheSecondReal0@users.noreply.github.com>
2021-02-27 00:06:49 -0500

    pasted charlie's selection box +added outline and transparency

--- src/games/pawn_game/main/world_ui.gd
+++ src/games/pawn_game/main/world_ui.gd
@@ -3,6 +3,9 @@ extends Node2D
 onready var camera: Camera2D = get_parent().get_node("pawn_game_cam")
 onready var interact_popup: Control = $CanvasLayer/interact_popup#$interact_popup
 
+var is_mouse_down: bool = false
+var mouse_down_pos: Vector2 = Vector2(0, 0)
+
 signal interaction_selected(interaction, tile)
 
 func _ready():
@@ -10,6 +13,28 @@ func _ready():
 	get_parent().connect("tile_created", self, "tile_created")
 # warning-ignore:return_value_discarded
 	interact_popup.connect("interaction_selected", self, "interaction_selected")
+
+func _draw():
+	if not is_mouse_down:
+		return
+	var mousePos = mouse_down_pos
+	var rect = Rect2(mousePos.x, mousePos.y, get_global_mouse_position().x - mousePos.x, get_global_mouse_position().y - mousePos.y)
+	draw_rect(rect, Color(.5, 1, 1, .25))
+	draw_rect(rect, Color(.5, 1, 1), false)
+
+func _unhandled_input(event):
+	if not event is InputEventMouse:
+		return
+	if event is InputEventMouseButton:
+		if not event.button_index == BUTTON_LEFT:
+			return
+		if event.pressed:
+			is_mouse_down = true
+			mouse_down_pos = get_global_mouse_position()#event.global_position
+		else:
+			is_mouse_down = false
+			return
+	update()
 
 func tile_interacted_with(tile: Node2D, input: InputEventMouseButton):
 	print(tile, " interacted with")
