2f32fc973d0fba2ab10834303fc1e33039215445
Asa Sprow <66881186+TheSecondReal0@users.noreply.github.com>
2021-02-27 01:06:44 -0500

    misc readability

--- src/games/pawn_game/resources/pawncommand/pawncommand.gd
+++ src/games/pawn_game/resources/pawncommand/pawncommand.gd
@@ -6,11 +6,11 @@ var order: Resource
 var pawn: KinematicBody2D
 
 # where the pawn should go
-var pawn_pos: Vector2
+var nav_target: Vector2
 # position of the object this command is interacting with
-# used so pawns can face the tile they're mining, could be used later to make pawns
-# 	watch a certain area if we implement view cones
-var target_pos: Vector2
+# used so pawns can face the tile they're mining or whatever, could be used later
+# 	to make pawns look somewhere if we implement view cones
+var command_pos: Vector2
 
 
 
